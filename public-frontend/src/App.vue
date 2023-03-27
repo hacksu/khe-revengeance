3eb4e2a01976a4465a367c7be9c33f10df58af6d
Mitch J <mjacovet@kent.edu>
2023-03-27 16:31:03 -0400

    create vite-ssg hello world for public frontend

---
+++ public-frontend/src/App.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>hmr?</h1>
+</template>
---
+++ public-frontend/src/main.js
@@ -0,0 +1,14 @@
+import { ViteSSG } from "vite-ssg";
+import App from "./App.vue";
+
+const routes = [{ path: "/", component: App }];
+
+// `export const createApp` is required instead of the original `createApp(App).mount('#app')`
+export const createApp = ViteSSG(
+  // the root component
+  App,
+  // vue-router options
+  { routes },
+  // function to have custom setups
+  ({ app, router, routes, isClient, initialState }) => {}
+);
