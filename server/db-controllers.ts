5b92dfae59dc8fd4926e5b37876952ea60c3c47f
Mitch J <mjacovet@kent.edu>
2023-03-29 15:41:00 -0400

    improve support ticket email formatting slightly

--- server/db-controllers.ts
+++ server/db-controllers.ts
@@ -7,15 +7,18 @@ mailer.setApiKey(secrets.sendgridKey);
 SupportTicketController.sendAlert = async function (ticket) {
   const msg: MailDataRequired = {
     to: secrets.supportEmailRecipient,
-    from: "khe-support-ticket@em3798.khe.io",
+    from: {
+      name: "KHE Support Ticket Alert",
+      email: "khe-support-ticket@em3798.khe.io",
+    },
     subject: "KHE SUPPORT TICKET: " + ticket.subject,
     text:
-      `from: ${ticket.theirName} at ${ticket.theirEmail}\n` +
-      `at: ${ticket.createdAt.toLocaleString()}\n` +
+      `from: ${ticket.theirName}, ${ticket.theirEmail}\n` +
+      `time: ${ticket.createdAt.toLocaleString()}\n` +
       `subject: ${ticket.subject}\n` +
-      `body: ${ticket.body}`,
+      `body:\n ${ticket.body}\n\n\n`,
     asm: {
-      groupId: 21989,
+      groupId: 21989, // ID for "KHE Support Ticket Alerts" on sendgrid
     },
   };
   const sendResult = await mailer.send(msg);
