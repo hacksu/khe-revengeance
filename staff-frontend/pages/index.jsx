afbec87e9f4c2d022bf90cfe2e4fe4f67b140d23
Mitch J <mjacovet@kent.edu>
2023-04-04 02:54:28 -0400

    add users card to staff site

--- staff-frontend/pages/index.jsx
+++ staff-frontend/pages/index.jsx
@@ -3,20 +3,30 @@ import KHELayout from "../layouts/layout.jsx";
 import { remult } from "remult";
 import { Email } from "../../global-includes/email-address.ts";
 import { Card, Statistic } from "antd";
+import { User } from "../../global-includes/users.ts";
+
+function DBCard({ children }) {
+    return <Card style={{ width: 200, margin: "10px 10px 10px 0" }}>{children}</Card>
+}
 
 function HomePage() {
 
     const [emailCount, setEmailCount] = useState(null);
+    const [userCount, setUserCount] = useState(null);
 
     useEffect(() => {
         remult.repo(Email).count().then(count => setEmailCount(count));
+        remult.repo(User).count().then(count => setUserCount(count));
     }, []);
 
     return <KHELayout>
         <div style={{ padding: "10px 0", height: "100%" }}>
-            <Card style={{ width: 200 }}>
-                <Statistic title="2023 Email Signups" value={emailCount || '...'} />
-            </Card>
+            <DBCard>
+                <Statistic title="Email Signups" value={emailCount ?? '...'} />
+            </DBCard>
+            <DBCard>
+                <Statistic title="User Accounts" value={userCount ?? '...'} />
+            </DBCard>
         </div>
     </KHELayout>;
 };
