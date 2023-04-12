039fbbe8f12829afd6bddbee640ba036f2302f10
Mitch J <mjacovet@kent.edu>
2023-04-12 02:29:52 -0400

    fix staff site dev performance a little

--- staff-frontend/next.config.js
+++ staff-frontend/next.config.js
@@ -1,2 +1,14 @@
-// they say this feature is experimental, but come on, it's been here since 2021
-export default { experimental: { externalDir: true } };
+export default {
+  // they say this feature is experimental, but come on, it's been here since 2021
+  experimental: { externalDir: true },
+  modularizeImports: {
+    "@ant-design/icons": {
+      transform: "@ant-design/icons/lib/icons/{{member}}",
+      preventFullImport: true,
+    },
+    antd: {
+      transform: "antd/lib/{{lowerCase (kebabCase member)}}",
+      preventFullImport: true,
+    },
+  },
+};
