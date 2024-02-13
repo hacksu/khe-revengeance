0704ee3c1207ce6e7a3c0f77cf4b9b36dbfb3bd6
Mitch J <mjacovet@kent.edu>
2024-02-12 23:51:05 -0500

    keep orbiter from blocking links

--- public-frontend/src/components/orbiter.vue
+++ public-frontend/src/components/orbiter.vue
@@ -28,7 +28,7 @@ export default {
         contSize() {
             return this.contWidth == -1 ?
                 { width: "100%", height: "100%" } :
-                { width: this.contWidth + "px", height: this.contWidth + "px" };
+                { width: this.contWidth + "px", height: "10px" };
         }
     }
 }
@@ -47,8 +47,9 @@ export default {
 
 .orbiter-container {
     position: absolute;
-    left: 0;
-    top: 0;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
 }
 
 .spinner {
