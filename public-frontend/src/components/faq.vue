9c61de338f9b789db4f7b4ef655940c84fe749f5
YoyoJesus <austinplaysmms12@gmail.com>
2024-11-14 10:54:53 -0500

    Revert "Updated FAQ"
    This reverts commit a0337fddcc7ca679723324b3839be6a3da0a8d30.

--- public-frontend/src/components/faq.vue
+++ public-frontend/src/components/faq.vue
@@ -3,20 +3,13 @@
     <div id="faq">
       <orbiter />
       <div class="info-container">
-        <h2 id="faqTitle">FREQUENTLY ASKED QUESTIONS</h2>
+        <h2 id="faqTitle">
+          FREQUENTLY ASKED QUESTIONS
+        </h2>
 
         <Accordion :multiple="true">
-          <AccordionTab
-            v-for="(faq, i) in faqs"
-            :key="i"
-            :header="faq.question"
-          >
-            <p
-              class="faq-answer"
-              v-for="(p, i) in faq.answer.split('\n\n')"
-              :key="i"
-              v-html="p"
-            />
+          <AccordionTab v-for="faq, i in faqs" :key="i" :header="faq.question">
+            <p class="faq-answer" v-for="p, i in faq.answer.split('\n\n')" :key="i" v-html="p" />
           </AccordionTab>
         </Accordion>
 
@@ -31,8 +24,8 @@
 
 <script>
 import orbiter from "./orbiter.vue";
-import Accordion from "primevue/accordion";
-import AccordionTab from "primevue/accordiontab";
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
 export default {
   name: "Main",
   components: { orbiter, Accordion, AccordionTab },
@@ -42,52 +35,57 @@ export default {
         {
           id: 0,
           question: "What is a hackathon?",
-          answer: `A hackathon is a weekend-long event to meet smart creative people, make connections, learn, and build something awesome!
+          answer:
+            `A hackathon is a weekend-long event to meet smart creative people, make connections, learn, and build something awesome! 
 
-            Attendees meet on Saturday, make a team, and have 24 hours to build a tech-related project.
-            There will be optional lessons and talks, sponsors looking to hire smart, motivated people, and tons of free stuff!`,
+            Attendees meet on Saturday, make a team, and have 24 hours to build a tech-related project. 
+            There will be optional lessons and talks, sponsors looking to hire smart, motivated people, and tons of free stuff!`
         },
         {
           id: 1,
           question: "Can I participate?",
-          answer: `If you're a college student of any kind, then yes!
-            KHE is attended by people of all skill levels and backgrounds.
-            You'll fit right in and hopefully, learn something!`,
+          answer:
+            `If you're a college student of any kind, then yes! 
+            KHE is attended by people of all skill levels and backgrounds. 
+            You'll fit right in and hopefully, learn something!`
         },
         {
           id: 2,
           question: "Sounds cool, but how much does it cost?",
-          answer: `$0.00!
-
-            Thanks to the generosity of our sponsors, we're able to put on events like this for <i>free</i>.
-            We even pay for your meals for the weekend!
-            Make sure to thank our sponsors - we couldn't do it without them!`,
+          answer:
+            `$0.00!
+            
+            Thanks to the generosity of our sponsors, we're able to put on events like this for <i>free</i>.  
+            We even pay for your meals for the weekend!  
+            Make sure to thank our sponsors - we couldn't do it without them!`
         },
         {
           id: 3,
           question: "What should I bring?",
-          answer: `Bring a laptop/something to hack on, a change of clothes, deodorant, a blanket/pillow, toiletries,
-            and anything else you think you might use to complete your project.`,
+          answer:
+            `Bring a laptop/something to hack on, a change of clothes, deodorant, a blanket/pillow, toiletries, 
+            and anything else you think you might use to complete your project.`
         },
         {
           id: 4,
           question: "Do I need to come with a team?",
           answer:
-            "You can, but you don't have to! We'll have team building at the event.",
+            "You can, but you don't have to! We'll have team building at the event."
         },
         {
           id: 5,
           question: "How many people per team?",
-          answer: "There's a hard limit of 4 participants per team.",
-        },
-      ],
+          answer:
+            "There's no hard limit, but the prizes are meant for teams of 4, so that's what we recommend."
+        }
+      ]
     };
   },
 };
 </script>
 
 <style scoped lang="scss">
-@import "@/globalVars.scss";
+@import '@/globalVars.scss';
 
 .info-container {
   padding: 20px;
@@ -96,22 +94,22 @@ export default {
 }
 
 :deep(.p-accordion .p-accordion-header) {
-  background-color: #1e1e1e !important;
+    background-color: #1e1e1e!important;
 }
 
 :deep(.p-accordion) {
-  background-color: black;
-  border: 2px solid white;
-  border-radius: 5px;
-  overflow: hidden;
+    background-color: black;
+    border: 2px solid white;
+    border-radius: 5px;
+    overflow: hidden;
 }
 
 :deep(.p-accordion .p-accordion-tab.p-accordion-tab-active) {
-  margin-bottom: 0;
+    margin-bottom: 0;
 }
 
 :deep(.p-accordion .p-accordion-content) {
-  background-color: #fff2;
+    background-color: #fff2;
 }
 
 #faq-container {
