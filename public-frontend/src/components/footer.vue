18d29227af5140c9dd59f80154e5f2e80d72b4d7
YoyoJesus <austinplaysmms12@gmail.com>
2024-11-14 10:53:03 -0500

    testing continued

--- public-frontend/src/components/footer.vue
+++ public-frontend/src/components/footer.vue
@@ -4,14 +4,14 @@
       POWERED BY
     </p>
 
-    <!--<a id="small-hacksu-logo" href="https://hacksu.com">
+    <a id="small-hacksu-logo" href="https://hacksu.com">
       <img style="opacity: 0.5" id="powered-by-hacksu" src="@/assets/2019-hacksu-color.svg" />
-    </a> -->
+    </a>
 
-    <a id="small-mlh-logo" href="https://github.com/MLH/mlh-policies/blob/main/code-of-conduct.md" v-if="mlh">
+    <!--<a id="small-mlh-logo" href="https://github.com/MLH/mlh-policies/blob/main/code-of-conduct.md" v-if="mlh">
       <img style="opacity: 0.5" id="powered-by-mlh" src="@/assets/mlh-logo.svg" />
       <span id="code-of-conduct-text">Code of Conduct</span>
-    </a>
+    </a>-->
 
     <div class="glow">
       <picture>
