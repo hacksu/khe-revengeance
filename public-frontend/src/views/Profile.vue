b709e574e6c02fcd5e540fec972481fbda4380b6
Cornelius Treadway Pavlic <cornelius.pavlic@gmail.com>
2025-01-14 18:05:32 -0500

    Fixed Registration misspellings

--- public-frontend/src/views/Profile.vue
+++ public-frontend/src/views/Profile.vue
@@ -44,7 +44,7 @@
                 </select>
             </div>
             <div class="field-row-stacked">
-                <label for="country">County of Residence <failureLabel v-if="submissionStatus == 'failed' && user.registration.country == ''"/></label>
+                <label for="country">Country of Residence <failureLabel v-if="submissionStatus == 'failed' && user.registration.country == ''"/></label>
                 <select id="country" v-model="user.registration.country">
                     <option v-for="(country, index) in countries" :key="index">{{country}}</option>
                 </select>
@@ -105,8 +105,9 @@
                 <input type="text" id="link" v-model="user.registration.link">
             </div><br>
             <div class="field-row-stacked">
+                <label for="resumeUpload">Upload Resume</label>
                 <button v-if="existingResume" @click="removeResume">Remove Resume</button><strong v-if="existingResume">{{existingResume}}</strong>
-                <input v-else type="file" url="/api/upload" accept="application/pdf" @select="fileChosen">
+                <input id="resumeUpload" v-else type="file" url="/api/upload" accept="application/pdf" @select="fileChosen">
             </div><br>
             <div class="radio-group">
                 <span>Is this your first hackathon?</span>
